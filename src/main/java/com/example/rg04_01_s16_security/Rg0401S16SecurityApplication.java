d9c9e26f2324771eea2d1bd58224d38418dd3d76
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-04-01 16:44:09 +0700

    Init

---
+++ src/main/java/com/example/rg04_01_s16_security/Rg0401S16SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.rg04_01_s16_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rg0401S16SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rg0401S16SecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rg04_01_s16_security/Rg0401S16SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rg04_01_s16_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rg0401S16SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
