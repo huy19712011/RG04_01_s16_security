625c04ef4c4cc36bf414212c7b85f8fc65b60b40
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-04-29 21:07:03 +0700

    287: Storing credentials using JDBC

--- src/main/java/com/example/rg04_01_s16_security/config/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/example/rg04_01_s16_security/config/BasicAuthSecurityConfiguration.java
@@ -4,13 +4,19 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -31,12 +37,42 @@ public class BasicAuthSecurityConfiguration {
         //disable CSRF
         http.csrf().disable();
 
+        // enable frames
+        http.headers().frameOptions().sameOrigin();
+
         http.httpBasic();
         return http.build();
     }
 
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        UserDetails user = User
+//                .withUsername("user")
+//                .password("{noop}123")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User
+//                .withUsername("admin")
+//                .password("{noop}123")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+
     @Bean
-    public UserDetailsService userDetailsService() {
+    public DataSource dataSource() {
+
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
 
         UserDetails user = User
                 .withUsername("user")
@@ -50,6 +86,12 @@ public class BasicAuthSecurityConfiguration {
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
+        JdbcUserDetailsManager jdbcUserDetailsManager =
+                new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
     }
+
 }
