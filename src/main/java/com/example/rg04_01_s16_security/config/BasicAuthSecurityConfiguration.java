083fedb60ddea63ef736cccc4a6269e49c3a2994
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-04-29 17:02:45 +0700

    286: Storing credentials in Memory

--- src/main/java/com/example/rg04_01_s16_security/config/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/example/rg04_01_s16_security/config/BasicAuthSecurityConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,8 +19,8 @@ public class BasicAuthSecurityConfiguration {
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> {
-                    auth.anyRequest().authenticated();
-                });
+            auth.anyRequest().authenticated();
+        });
 //        http.formLogin();
 
         // disable session
@@ -29,5 +33,23 @@ public class BasicAuthSecurityConfiguration {
 
         http.httpBasic();
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User
+                .withUsername("user")
+                .password("{noop}123")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("{noop}123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
